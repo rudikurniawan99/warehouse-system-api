fde3990e54ea4cafcd36edd8a6ccfdbbb2f37643
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-27 16:14:30 +0700

    create User model

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,8 @@ require('dotenv').config('../.env')
 import express from 'express'
 import connectDB from './utils/connectDB'
 
-
-const app = express()
-
 const port = process.env.PORT
+const app = express()
 
 app.listen(port || 4000, () => {
   console.log(`app running at http://localhost:${port}`);
---
+++ src/models/user.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Document } from 'mongoose'
+import argon from 'argon2'
+
+export interface UserInput {
+  name: string
+  email: string
+  password: string
+}
+
+export interface UserDocument extends UserInput, Document {
+  createdAt: Date
+  updatedAt: Date
+  verifyPassword: (passwordInput: string) => Promise<boolean>
+}
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.pre('save', async function(next){
+  let user = this as UserDocument
+  if(!user.isModified('passsword')){
+    next()
+  }
+  const hashPassword = await argon.hash(user.password)
+  user.password = hashPassword
+})
+
+userSchema.methods.verifyPassword = async function(passwordInput: string): Promise<boolean>{
+  let user = this as UserDocument
+  try {
+    return argon.verify(user.password, passwordInput)
+  } catch (e: any) {
+    return false 
+  }
+}
+
+export const User = mongoose.model<UserDocument>('User', userSchema)
+export default User

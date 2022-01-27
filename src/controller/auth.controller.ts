e53d05fc702b5dbb979cad0c97bff7c40e0ceca8
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-27 18:39:42 +0700

    edit auth.controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
+import { UserInput } from "../models/user.model";
+import { createUser } from "../services/auth.services";
 
-export const registerUserHandler = (req: Request, res: Response) => {
+export const registerUserHandler = async (req: Request<{}, {}, UserInput>, res: Response) => {
   const { body } = req
-  // console.log(body)
 
-  res.status(201).json(body)
+  try {
+    const user = await createUser(body)
+    res.status(201).json({
+      data: user
+    })
+  } catch (e) {
+    res.sendStatus(400) 
+  }
+
 }

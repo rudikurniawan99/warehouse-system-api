8e28fafae241ca74a0f5f635a14fb79d925aea9f
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-30 16:16:01 +0700

    create product model

---
+++ src/models/product.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from 'mongoose' 
+import { UserDocument } from './user.model'
+
+interface ProductInput {
+  admin: UserDocument['_id']
+  name: string
+
+
+}
+
+interface ProductDocument extends ProductInput, Document {
+  createdAt: Date
+  updatedAt: Date
+} 
+
+const productSchema = new mongoose.Schema({
+  admin: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+  size: {
+    type: [Schema.Types.ObjectId],
+    default: []
+  }
+}, {
+  timestamps: true
+})
+
+const Product = mongoose.model<ProductDocument>('Product', productSchema)
+export default Product

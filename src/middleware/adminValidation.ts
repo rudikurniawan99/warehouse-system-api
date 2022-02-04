dc1331973f1857f6f3affaabdff1efce2a151d10
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-02-04 13:55:19 +0700

    create admin validation middleware

--- src/middleware/adminValidation.ts
+++ src/middleware/adminValidation.ts
@@ -20,6 +20,16 @@ const ownerAdminValidation = async (req: Request, res: Response, next: NextFunct
   } 
 }
 
+const commonAdminValidation = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization']
+  const accessToken = authHeader && authHeader.split(' ')[1]
+  try {
+    if(!accessToken) return res.sendStatus(401)
+    const decoded = await jwtVerify<AccessTokenPayload>(accessToken, accessTokenPrivateKey) 
+    if(decoded?.admin_status === 'ADMIN') next()
+  } catch (error) {
+    return res.sendStatus(402) 
+  }
+}
 
-
-export { ownerAdminValidation }
+export { ownerAdminValidation, commonAdminValidation }

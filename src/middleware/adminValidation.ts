cc0f2f299c8a384687b9d0046d568c262e0adcbb
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-02-04 13:54:12 +0700

    create ownerAdminValidation middleware

---
+++ src/middleware/adminValidation.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { jwtVerify } from "../utils/jwt";
+
+interface AccessTokenPayload {
+  _id: string
+  admin_status: 'OWNER' | 'ADMIN'
+}
+
+const accessTokenPrivateKey = String(process.env.ACCESS_TOKEN_PRIVATE_KEY)
+
+const ownerAdminValidation = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization']
+  const accessToken = authHeader && authHeader.split(' ')[1]
+  try {
+    if(!accessToken) return res.sendStatus(401)
+    const decoded  = await jwtVerify<AccessTokenPayload>(accessToken, accessTokenPrivateKey)
+    if(decoded?.admin_status === 'OWNER') next()
+  } catch (error) {
+    return res.sendStatus(402) 
+  } 
+}
+
+
+
+export { ownerAdminValidation }

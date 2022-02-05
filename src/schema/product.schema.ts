0bc4dc87c5218e847a82c76c574625348b049bdd
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-02-05 08:45:39 +0700

    delete adminId property from createProductSchema validation

--- src/schema/product.schema.ts
+++ src/schema/product.schema.ts
@@ -2,9 +2,6 @@ import { number, object, string } from "zod";
 
 export const createProductSchema = object({
   body: object({
-    adminId: string({
-      required_error: 'required adminId'
-    }),
     name: string({
       required_error: 'required name'
     }),
